6bb9cad59b18f749f71e6641d9694f7c50965025
emirkoz7 <dani7barca@gmail.com>
2024-10-01 16:26:50 +0100

    Update index.tsx

--- src/components/HomeListAll/index.tsx
+++ src/components/HomeListAll/index.tsx
@@ -457,7 +457,7 @@ const HomeListAll = () => {
             onClick={() => {
               document
                 .querySelectorAll(`.${styles.HomeListSection}`)
-                [recommendations.length > 0 ? 3 : 4].scrollBy(-700, 0);
+                [recommendations.length > 0 ? 3 : 2].scrollBy(-700, 0);
             }}
             data-tooltip-id="tooltip"
             data-tooltip-content="Swipe Left"
@@ -466,7 +466,7 @@ const HomeListAll = () => {
             onClick={() => {
               document
                 .querySelectorAll(`.${styles.HomeListSection}`)
-                [recommendations.length > 0 ? 3 : 4].scrollBy(700, 0);
+                [recommendations.length > 0 ? 3 : 2].scrollBy(700, 0);
             }}
             data-tooltip-id="tooltip"
             data-tooltip-content="Swipe Right"
@@ -489,7 +489,7 @@ const HomeListAll = () => {
             onClick={() => {
               document
                 .querySelectorAll(`.${styles.HomeListSection}`)
-                [recommendations.length > 0 ? 5 : 6].scrollBy(-700, 0);
+                [recommendations.length > 0 ? 4 : 3].scrollBy(-700, 0);
             }}
             data-tooltip-id="tooltip"
             data-tooltip-content="Swipe Left"
@@ -498,7 +498,7 @@ const HomeListAll = () => {
             onClick={() => {
               document
                 .querySelectorAll(`.${styles.HomeListSection}`)
-                [recommendations.length > 0 ? 5 : 6].scrollBy(700, 0);
+                [recommendations.length > 0 ? 4 : 3].scrollBy(700, 0);
             }}
             data-tooltip-id="tooltip"
             data-tooltip-content="Swipe Right"
@@ -521,7 +521,7 @@ const HomeListAll = () => {
             onClick={() => {
               document
                 .querySelectorAll(`.${styles.HomeListSection}`)
-                [recommendations.length > 0 ? 7 : 8].scrollBy(-700, 0);
+                [recommendations.length > 0 ? 5 : 4].scrollBy(-700, 0);
             }}
             data-tooltip-id="tooltip"
             data-tooltip-content="Swipe Left"
@@ -530,7 +530,7 @@ const HomeListAll = () => {
             onClick={() => {
               document
                 .querySelectorAll(`.${styles.HomeListSection}`)
-                [recommendations.length > 0 ? 7 : 8].scrollBy(700, 0);
+                [recommendations.length > 0 ? 5 : 4].scrollBy(700, 0);
             }}
             data-tooltip-id="tooltip"
             data-tooltip-content="Swipe Right"
