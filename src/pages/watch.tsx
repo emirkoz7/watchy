fad5cc1bc92373b2653a75d6e204235798c1b673
emirkoz7 <dani7barca@gmail.com>
2024-10-30 18:57:46 +0100

    Update watch.tsx

--- src/pages/watch.tsx
+++ src/pages/watch.tsx
@@ -650,7 +650,7 @@ const Watch = () => {
             allowFullScreen
             allow="accelerometer; autoplay; encrypted-media; gyroscope;"
             referrerPolicy="origin"
-			sandbox="allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation allow-presentation"
+
           ></iframe>
         ) : null}
 
@@ -716,7 +716,7 @@ const Watch = () => {
             allowFullScreen
             allow="accelerometer; autoplay; encrypted-media; gyroscope;"
             referrerPolicy="origin"
-	    sandbox="allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation allow-presentation"  
+
           ></iframe>
         ) : null}
 
